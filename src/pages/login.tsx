9d120341807414d44ec4a89bbcd43b1949cd4df8
HomieKim <ghals0708@gmail.com>
2022-07-19 00:28:20 +0900

    design: NavBar & Header

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Test = styled.div`
+  width :100px;
+  height : 150px;
+  background-color : grey;
+  border-radius: 0px -30px;
+`
 
 const LoginPage = () => {
-  
+
   return (
-    <button>카카오 로그인</button>
+    <Test>df</Test>
   )
 }
 
